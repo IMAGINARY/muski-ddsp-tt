067c27295b9fc7ff8b95421f78a588bb82e09041
Eric Londaits <eric.londaits@gmail.com>
2023-01-18 09:38:18 -0300

    Implement UI modes, with loaders and button disabling.

--- src/js/muski-ddsp-tt-ui.js
+++ src/js/muski-ddsp-tt-ui.js
@@ -18,6 +18,7 @@ export default class MusKiDDSPTTUI {
 
     this.$status = $('<div></div>')
       .addClass(['alert', 'alert-secondary', 'muski-toy-status'])
+      .hide()
       .appendTo(this.$element);
 
     this.$recordPane = $('<div></div>')
@@ -39,6 +40,15 @@ export default class MusKiDDSPTTUI {
         .addClass('bar'))
       .appendTo(this.$recordPane);
 
+    this.$recordSpinner = $('<div></div>')
+      .addClass(['spinner-border', 'text-danger', 'record-spinner'])
+      .append($('<span></span>')
+        .addClass('visually-hidden')
+        .text('Processing recording...'))
+      .appendTo(this.$recordPane);
+
+    this.hideRecordingSpinner();
+
     this.$toneButtons = Object.keys(this.options.models).map((model) => (
       $('<button></button>')
         .addClass(['btn', 'mx-2', 'mb-2', 'muski-ddsp-tt-tone', `muski-ddsp-tt-tone-${model}`])
@@ -52,13 +62,28 @@ export default class MusKiDDSPTTUI {
     this.$toneButtonPanel = $('<div></div>')
       .addClass('tone-buttons text-center mb-3')
       .append(this.$toneButtons)
+      .append(this.$toneSpinner)
       .appendTo(this.$element)
       .hide();
 
+    this.$audioPane = $('<div></div>')
+      .addClass('audio-pane')
+      .appendTo(this.$element);
+
     this.$player = $('<audio></audio>')
       .attr('controls', 'controls')
-      .css({ display: 'none' })
-      .appendTo(this.$element);
+      .appendTo(this.$audioPane);
+
+    this.hidePlayer();
+
+    this.$audioSpinner = $('<div></div>')
+      .addClass(['spinner-border', 'audio-spinner'])
+      .append($('<span></span>')
+        .addClass('visually-hidden')
+        .text('Transferring tone...'))
+      .appendTo(this.$audioPane);
+
+    this.hideAudioSpinner();
 
     this.ddsptt.events.on('initStart', () => {
       this.onInitStart();
@@ -69,12 +94,55 @@ export default class MusKiDDSPTTUI {
       this.onStatus(message);
     }).on('toneTransferred', (url) => {
       this.$player.attr('src', url);
-      this.$player.css({ display: 'block' });
+      this.hideAudioSpinner();
+      this.showPlayer();
+      this.$player[0].play();
     });
+
+    this.disableToneButtons();
   }
 
   setStatus(message) {
     this.$status.text(message);
+  }
+
+  disableToneButtons() {
+    this.$toneButtons.forEach((button) => {
+      button.attr('disabled', 'disabled').addClass('disabled');
+    });
+  }
+
+  enableToneButtons() {
+    this.$toneButtons.forEach((button) => {
+      button.removeAttr('disabled').removeClass('disabled');
+    });
+  }
+
+  hidePlayer() {
+    this.$player[0].pause();
+    this.$player.css({ visibility: 'hidden' });
+  }
+
+  showPlayer() {
+    this.$player.css({ visibility: 'visible' });
+  }
+
+  showRecordingSpinner() {
+    this.$recordButton.addClass('busy');
+    this.$recordSpinner.show();
+  }
+
+  hideRecordingSpinner() {
+    this.$recordButton.removeClass('busy');
+    this.$recordSpinner.hide();
+  }
+
+  showAudioSpinner() {
+    this.$audioSpinner.show();
+  }
+
+  hideAudioSpinner() {
+    this.$audioSpinner.hide();
   }
 
   async init() {
@@ -103,6 +171,8 @@ export default class MusKiDDSPTTUI {
       this.isRecording = true;
       this.$recordButton.addClass('recording');
       this.$recordBar.addClass('recording');
+      this.disableToneButtons();
+      this.hidePlayer();
       this.recordingTimeout = setTimeout(() => {
         this.stopRecording();
       }, MAX_RECORDING_LEN);
@@ -123,9 +193,9 @@ export default class MusKiDDSPTTUI {
   async handleToneButton(model) {
     if (!this.isBusy) {
       this.isBusy = true;
-      this.$player.hide();
+      this.hidePlayer();
+      this.showAudioSpinner();
       await this.ddsptt.toneTransfer(this.options.models[model]);
-      this.$player.show();
       this.isBusy = false;
     }
   }
@@ -145,7 +215,9 @@ export default class MusKiDDSPTTUI {
   async onRecording(recording) {
     this.lastRecording = recording;
     this.setStatus('Recording done.');
-    console.log(this.lastRecording.blobUrl, this.lastRecording.mimeType, this.lastRecording.size);
+    this.showRecordingSpinner();
     await this.ddsptt.loadAudio(this.lastRecording.blobUrl);
+    this.enableToneButtons();
+    this.hideRecordingSpinner();
   }
 }
